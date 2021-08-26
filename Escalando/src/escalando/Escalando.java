75e7626904f326c54ac9cc03ceb992e44e0afff2
sthephan <stevs.warrior@gmail.com>
2021-08-26 00:51:21 -0600

    agregado a github 1er 26/8/2021

---
+++ Escalando/src/escalando/Escalando.java
@@ -0,0 +1,120 @@
+package escalando;
+import java.util.Random;
+import java.util.Scanner;
+/*
+ * @author sthephan garcia 200915522
+ */
+public class Escalando {
+    
+    boolean partida= false;//validador si hay partida guardada
+    String valores[];//valores
+    String matrix[][]= new String[8][8];
+    
+    public Escalando(){
+        inicio();
+    
+    }
+    
+    public static void main(String[] args) {
+        new Escalando();
+    }    
+    
+    public void inicio(){
+        //Menu incial del prograna
+        System.out.println("**************Escalando**************");
+        System.out.println("==============  Menu  ===============");
+        System.out.println("* 1. Iniciar juego                  *");
+        System.out.println("* 2. Retomar juego                  *");
+        System.out.println("* 3. Generar reportes               *");
+        System.out.println("* 4. Salir                          *");
+        System.out.println("*************************************");
+        System.out.print("Ingrese la opción que desea ejecutar:");                
+        Scanner opcion = new Scanner(System.in);        
+        
+        try{                        
+            int ingresado=opcion.nextInt();
+            switch(ingresado){
+                case 1:
+                    if(partida==false){//validar si hay un juego guaradado con un if                         
+                        
+                        for(int i=0;i<8;i++){
+                            
+                            //dar un numero aleatorio entre 2 y 4 aleatoria entre cada fila
+                            int prob=0;
+                            Random probabilidad_penalizacion= new Random();
+                            prob=2+probabilidad_penalizacion.nextInt(2);
+
+                            //dar las posiciones aleatorias en la fila, ls cuales seran las penalizaciones
+                            int posiciones[]= new int[(prob)];
+
+                            
+                            //usamos un while para validar que hagan numeros repetidos
+                            int contador=0;
+                            while(contador<8){
+                            
+                            }
+                            
+                            
+                            for(int j=0;j<8;j++){
+                                
+                            }
+                            
+                        
+                        
+                        }
+                        
+                        juego();
+                    }else{
+                        System.out.println("¡Hay una partida alamacenada!,¿Desea descartarla? Si(s),No(n)");
+                        
+                        //codigo de descartar partida
+                    }                    
+                    break;
+                case 2:
+                    if(partida==true){//validar si hay un juego guardado tambiem
+
+                    }else{
+
+                    };
+                    break;
+                case 3:
+                    System.exit(0);                
+            }                        
+        }catch(Exception e){
+            System.out.println("¡opción ingresada invalida!\n");
+            inicio();            
+        }
+        
+        
+    }
+    
+    
+    
+    public void juego(){
+        System.out.println("¿Que empieze el reto!\n");
+        System.out.println("-------------------------------------------");
+        System.out.println("|      |      |      |      |      |      |");
+        System.out.println("|      |      |      |      |      |      |");
+        System.out.println("-------------------------------------------");
+        System.out.println("|      |      |      |      |      |      |");
+        System.out.println("|      |      |      |      |      |      |");
+        System.out.println("-------------------------------------------");
+        System.out.println("|      |      |      |      |      |      |");
+        System.out.println("|      |      |      |      |      |      |");
+        System.out.println("-------------------------------------------");
+        System.out.println("|      |      |      |      |      |      |");
+        System.out.println("|      |      |      |      |      |      |");
+        System.out.println("-------------------------------------------");
+        System.out.println("|      |      |      |      |      |      |");
+        System.out.println("|      |      |      |      |      |      |");
+        System.out.println("-------------------------------------------");
+        System.out.println("|      |      |      |      |      |      |");
+        System.out.println("|      |      |      |      |      |      |");
+        System.out.println("-------------------------------------------");
+        
+    }
+    
+    public void juego(String valores[]){
+    
+    }
+}
---
+++ Parcial1/src/parcial1/Parcial1.java
@@ -0,0 +1,44 @@
+/*Parte 2 del parcial 1 ipc1d 19/agosto/2021*/
+package parcial1;
+/**
+ * @author sthephan garcia 200915522
+ */
+public class Parcial1 {    
+    public Parcial1(){        
+        //procesarCalculo();    
+    }
+    
+    public static void main(String[] args) {
+        new Parcial1();
+        
+    }
+    
+   
+    
+    
+}
+
+
+ //int j;//para solventar el erro de compilador declare esta variable ja nivel global
+
+/*public void procesarCalculo() {
+        int i, k, j, modulo,x;
+        x=10;//asigene un valor de 10 a x ya que necesita un valor
+        modulo=0;//asigne un valor de modulo 0 para poder observar el rsultado de switch el case de 1
+        for (i = 0; i < 2; i++) {
+            switch(i-modulo){
+                case 0:
+                    j = 45;// remover para quitar error de compilador
+                    this.j=15;
+                    System.out.println(this.j);
+                    System.out.println(j);
+                break;
+                case 1:
+                    System.out.println(x);
+                break;
+                default:
+                    k = j=i;
+                break;
+            }
+        }
+    }    */

31244247c782504447222c11fc58a499596b37bc
sthephan <stevs.warrior@gmail.com>
2021-09-04 16:16:28 -0600

    9/4/2021 2

--- Proyecto1/src/proyecto1/Proyecto1.java
+++ Proyecto1/src/proyecto1/Proyecto1.java
@@ -1,21 +1,24 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+/**
+ * Esta clase maneja todo el software es la "main"
+ * @sthephan Alexander Garcia Marroquin 200915522
+ * @version 1.0
+ */ 
 package proyecto1;
 
-/**
- *
- * @author sthephan
- */
-public class Proyecto1 {
+//Campos de clase
 
+public class Proyecto1 {
+    
     /**
-     * @param args the command line arguments
+     * Constructor base cuando el software se inicia sin parametros     
      */
+    public Proyecto1(){
+    
+    }//cierre de constructor
+    
+
     public static void main(String[] args) {
         // TODO code application logic here
     }
     
-}
+}//cierre ded clase Proyecto1
